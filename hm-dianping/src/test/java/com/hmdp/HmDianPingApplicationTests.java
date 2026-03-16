9d5b6b604c70cdf33b0881272070525a59a53ff8
Zycx14 <yangchenxuz@student.unimelb.edu.au>
2026-03-16 16:33:11 +0800

    final

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,12 +1,6 @@
 package com.hmdp;
 
-import com.hmdp.utils.SimpleRedisLock;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import javax.annotation.Resource;
-import java.util.concurrent.CountDownLatch;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
